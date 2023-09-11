13f810be3258603aaf8f4ef7153ef26b7bcce55f
Egor <doesnoexist@gmail.com>
2023-09-11 22:16:42 +0700

    update task 0301

--- 03/1.c
+++ 03/1.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     unsigned int n;
     scanf("%d", &n);
-    if (n%3==0) printf("fizz ");
-    if (n%5==0) printf("buzz ");
+    for (unsigned int i = 0; i <= n; i++) {
+        if (i % 3 != 0 && i % 5 != 0) {
+            printf("%d ", i);
+            continue;
+        }
+        if (i % 3 == 0) printf("fizz ");
+        if (i % 5 == 0) printf("buzz ");
+    }
     return 0;
 }
