a263e978b5ee07b45feba3ccbc1248aa0f8a2072
Egor <doesnoexist@gmail.com>
2023-09-11 17:38:41 +0700

    harderbetterfasterstronger

--- 03/4.c
+++ 03/4.c
@@ -2,14 +2,14 @@
 #include <math.h>
 
 int main(){
-    unsigned long long n;
+    unsigned long long n, r;
     scanf("%lld", &n);
     if (n<2){
         printf("Число должно быть больше 1");
         return 0;
     }
-
-    for(unsigned long long int i = 2; i<ceil(sqrt(n)); i++){ //заменить на sqrt
+    r = ceil(sqrt(n));
+    for(unsigned long long int i = 2; i<r; i++){ //заменить на sqrt
         if (n%i==0){
             printf("Непростое");
             return 0;
