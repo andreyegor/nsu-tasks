c018a67f900315ebae9c36a29eaf94bee97a5488
Egor <doesnoexist@gmail.com>
2023-09-18 12:56:59 +0700

    02/1 fix

--- 02/2.c
+++ 02/2.c
@@ -3,6 +3,14 @@
 int main(){
     unsigned short year;
     scanf("%hi", &year);
-    if (year%4==0) printf("Високосный"); else printf("Невисокосный");
+    if (year%400==0){
+        printf("Високосный");
+    } else if (year%100==0){
+        printf("Невисокосный");
+    } else if (year%4==0){
+        printf("Високосный");
+    } else{
+        printf("Невисокосный");
+    }
     return 0;
 }
