1ec09135e318bd84894e9c62f72cc994dc646a07
Egor <doesnoexist@gmail.com>
2023-09-18 14:04:47 +0700

    all funcs

--- 06/0.c
+++ 06/0.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 void scanArr(int* arr, size_t len) {
     for (size_t i = 0; i < len; i++) {
@@ -15,17 +16,50 @@ void printArr(int* arr, size_t len) {
 
 void revertArr(int* arr, size_t len) {
     int tmp;
-    for (size_t i = 0; i<len/2; i++){
+    for (size_t i = 0; i < len / 2; i++) {
         tmp = arr[i];
-        arr[i] = arr[len-i-1];
-        arr[len-i-1] = tmp;
-    } 
+        arr[i] = arr[len - i - 1];
+        arr[len - i - 1] = tmp;
+    }
 }
 
-void maxlnArr(int* arr, size_t len){
+void maxlnArr(int* arr, size_t len) {
     int mx = 0;
-    for (size_t i = 0; i<len; i++){
-        if arr[i]>mx mx = arr[i];
-        
+    for (size_t i = 0; i < len; i++) {
+        if (arr[i] > mx) mx = arr[i];
     }
+}
+
+size_t findlnArr(int* arr, size_t len, int x) {
+    for (size_t i = 0; i < len; i++) {
+        if (arr[i] == x) return i;
+    }
+}
+
+void findlnArr(int* arr, size_t len, int x) {
+    assert(len < 10);
+    for (size_t i = 0; i < len; i++) {
+        arr[i] = x;
+    }
+}
+
+void findlnArr(int* arr, size_t len, int x) {
+    int x_len = 0, x_tmp = x;
+    while (x_tmp){
+        x_len+=1;
+        x_tmp/=10;
+    }
+    for (int i=0; i<=x_len;i++){
+        arr[i] = x%i/pow(10,x_len-i-1); // я в этом не уверен и деление на ноль ещё есть
+    }
+}
+
+int compareArrays(int* arr1, size_t len1, int* arr2, size_t len2) {
+    size_t len = (len1 > len2) ? len1 : len2;
+    for (size_t i = 0; i < len; i++) {
+        if (arr1[i] < arr2[i]) return -1;
+        if (arr1[i] > arr2[i]) return 1;
+    }
+    if (len1 = len2) return 0;
+    return (len1 < len2) ? -1 : 1;
 }
