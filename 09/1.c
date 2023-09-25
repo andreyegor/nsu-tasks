fe63dbcc9e42e2556b03468b331600e2d173701c
Egor <doesnoexist@gmail.com>
2023-09-25 13:15:39 +0700

    09/1 unchecked

--- 09/1.c
+++ 09/1.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 void concat(int* arr1, size_t len1, int* arr2, size_t len2, int* out_arr, size_t* out_len) {
-    *out_len = len1+len2;
-    for(int i=0; i<len1; i++) out_arr[i] = arr1[i];
-    for(int i=0; i<len2; i++) out_arr[len1+i] = arr2[i];
+    *out_len = len1 + len2;
+    for (int i = 0; i < len1; i++) out_arr[i] = arr1[i];
+    for (int i = 0; i < len2; i++) out_arr[len1 + i] = arr2[i];
 }
 
-int main(){
+int main() {
     size_t len1, len2;
     scanf("%lld", &len1);
     int arr1[len1];
-    for(size_t i=0;i<len1;i++) scanf("%d", &arr1[i]);
-    
+    for (size_t i = 0; i < len1; i++) scanf("%d", &arr1[i]);
+
     scanf("%lld", &len2);
     int arr2[len2];
-    for(size_t i=0;i<len2;i++) scanf("%d", &arr2[i]);
+    for (size_t i = 0; i < len2; i++) scanf("%d", &arr2[i]);
 
     size_t out_len;
-    int out_arr[len1+len2];
+    int out_arr[len1 + len2];
     concat(arr1, len1, arr2, len2, out_arr, &out_len);
     printf("%d\n", out_len);
-    for(size_t i=0;i<out_len;i++) printf("%d ", out_arr[i]);
+    for (size_t i = 0; i < out_len; i++) printf("%d ", out_arr[i]);
     return 0;
 }
