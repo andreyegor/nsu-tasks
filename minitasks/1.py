49d1da277bd0799d6401c24b7c2de47af8338133
Egor <doesnoexist@gmail.com>
2023-09-28 01:16:58 +0700

    miracle

--- minitasks/1.py
+++ minitasks/1.py
@@ -1,10 +1,9 @@
 n = int(input())
-cnt = n&1 if n!=-1 else 2
+cnt = n&1
 
 while n not in (0,-1):
-    #чудеса
     n>>=1
     cnt+=n&1
-    print(bin(n), n&1)
+    print(n, n&1)
 
-print(cnt)
+print(2 if cnt == 1 and n==-1 else cnt)
