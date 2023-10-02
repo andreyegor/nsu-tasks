38bf22d28d18b4493b9ad47c63a6577996b3a844
Egor <doesnoexist@gmail.com>
2023-10-02 13:07:02 +0700

    mini 4

---
+++ minitasks/4.py
@@ -0,0 +1,14 @@
+def reversed_dict(d):
+    out = dict()
+    for key in d:
+        # d[hashable] сразу вернёт TypeError, по этому доп. кода не нужно
+        if d[key] not in out:
+            out[d[key]] = key
+        elif type(d[key]) != tuple:
+            out[d[key]] = (out[d[key]], key)
+        else:
+            out[d[key]] += (key,)
+    return out
+
+
+print(reversed_dict({"Ivanov": [97832], "Petrov": 55521, "Sidorov": 97832}))
