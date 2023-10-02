1dbbeca10f6cfcb3bd1690afbc713a4b1bdc76f2
Egor <doesnoexist@gmail.com>
2023-10-02 13:20:38 +0700

    mini 3

---
+++ minitasks/3.py
@@ -0,0 +1,9 @@
+def matrix(data):
+    out = []
+    for line in data.split("|"):
+        out.append([])
+        for e in line.split():
+            out[-1].append(float(e))
+    return out
+
+print(matrix("1 2| 3 4"))
