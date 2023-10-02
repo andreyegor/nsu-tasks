923df06e223c3695fd821825aa884951eb0d9187
Egor <doesnoexist@gmail.com>
2023-10-02 13:18:57 +0700

    mini 2

--- minitasks/2.py
+++ minitasks/2.py
@@ -1,4 +1,6 @@
 def my_zip(a, b):
-    return ((a[i], b[i]) for i in range(min(len(a), len(b)))) #сразу ленивая функция, возвращаю генератор
+    # сразу ленивая функция, возвращаю генератор
+    return ((a[i], b[i]) for i in range(min(len(a), len(b))))
 
-print(list(my_zip([1,2,3], ['a','b'])))
+
+print(list(my_zip([1, 2, 3], ["a", "b"])))
