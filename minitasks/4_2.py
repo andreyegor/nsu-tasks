18bd29b4402039e3aaca9fa9b36ea473e63c6b15
Egor <doesnoexist@gmail.com>
2023-10-06 20:41:29 +0700

    4_2 updated

--- minitasks/4_2.py
+++ minitasks/4_2.py
@@ -1,17 +1,19 @@
 def reversed_dict(d):
+    keys = set()
     is_tupple = set()
     out = dict()
     for key, val in d.items():
-        if val not in out.keys():
+        if val not in keys:
             out[val] = key
+            keys.add(key)
         elif val not in is_tupple:
             out[val] = (out[val], key)
             is_tupple.add(val)
         else:
-            out[val] += (key)
+            out[val] += key
     return out
-            
-        
+
+
 print(reversed_dict({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832}))
 print(reversed_dict({"Ivanov": (97832, 2), "Petrov": 55521, "Kuznecov": (97832, 2)}))
 print(reversed_dict({("Ivanov", 1): (97832, 2), "Petrov": 55521, ("Kuznecov", 1): (97832, 2)}))
