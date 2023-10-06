229d4d68159afc42cbd6b1311f90f9fb0a8bd554
Egor <doesnoexist@gmail.com>
2023-10-06 20:27:11 +0700

    4 formatted

--- minitasks/4.py
+++ minitasks/4.py
@@ -2,17 +2,22 @@ from collections import defaultdict
 
 
 def reversed_dict(d):
-    out = defaultdict(lambda:[])
+    out = defaultdict(lambda: [])
     # out[hashable] сразу вернёт TypeError, по этому доп. кода не нужно
     for key in d:
         out[d[key]].append(key)
     for key in out:
-        if len(out[key])==1:
+        if len(out[key]) == 1:
             out[key] = out[key][0]
         else:
             out[key] = tuple(out[key])
     return dict(out)
 
+
 print(reversed_dict({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832}))
 print(reversed_dict({"Ivanov": (97832, 2), "Petrov": 55521, "Kuznecov": (97832, 2)}))
-print(reversed_dict({("Ivanov", 1): (97832, 2), "Petrov": 55521, ("Kuznecov", 1): (97832, 2)}))
+print(
+    reversed_dict(
+        {("Ivanov", 1): (97832, 2), "Petrov": 55521, ("Kuznecov", 1): (97832, 2)}
+    )
+)
