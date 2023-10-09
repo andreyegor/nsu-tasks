75cf4c3b4729e3e5f293575e7c231a522680a894
Egor <doesnoexist@gmail.com>
2023-10-09 16:49:40 +0700

    other version

--- minitasks/5_2.py
+++ minitasks/5_2.py
@@ -5,6 +5,11 @@ def specialize(f, *spec_args, **spec_kwargs):
     """В этой реализации можно переопределять аргументы как угодно, а ещё менять переопределённые значения итоговой функции"""
 
     spec_kwargs.update(**dict(zip(f.__code__.co_varnames, spec_args)))
+    
+    # альтернативная версия альтернативной реализации, и я без понятия как будет лучше - при работе с f(a,b) 
+    # код ниже разберёт specialize(f,1,a=2) как а=2 b=1, тогда как код выше разберёт те же данные как a=1
+    # spec_kwargs.update(**dict(zip(
+    # filter(lambda x: x not in spec_kwargs,f.__code__.co_varnames), spec_args)))
 
     def spec_f(*args, **kwargs):
         spec_kwargs.update(kwargs)
@@ -15,7 +20,7 @@ def specialize(f, *spec_args, **spec_kwargs):
     return spec_f
 
 # в первой реализации нельзя сделать во так
-plus_one = specialize(sum, 1, a=1)
+plus_one = specialize(sum, a=1)
 print(plus_one(10))
 # и вот так
 print(plus_one(10, a=5))
