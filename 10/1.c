3ee6e990659cf55b643b303ba46428ca2e5c7deb
Egor <doesnoexist@gmail.com>
2023-10-10 17:04:57 +0700

    10/1

---
+++ 10/1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int palindrom(const char* line) {
+    size_t right = strlen(line);
+
+    for (int left = 0; left <= right / 2; left++) {
+        right--;
+        if (line[left] == ' ')
+            right++;
+        else if (line[right] == ' ')
+            left--;
+        else if (line[left] != line[right])
+            return 0;
+    }
+    return 1;
+}
+
+int main() {
+    char line[128];
+
+    scanf("%[^\n]", line);
+    printf("%s", palindrom(line) ? "Yes" : "No");
+    return 0;
+}
