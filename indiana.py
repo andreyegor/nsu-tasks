b8d9a07e06912f1019102f70950898e809376cf1
Egor <doesnoexist@gmail.com>
2023-10-23 17:12:59 +0700

    version 3 old input

--- indiana.py
+++ indiana.py
@@ -3,15 +3,15 @@ import indiana_test
 
 
 def solution(data: str) -> int:
-    lines = data.splitlines()
-    power, q_nodes, q_edges = [int(e) for e in lines[0].split()]
+    lines = ([int(q) for q in e.split()] for e in data.splitlines())
+    power, q_nodes, q_edges = next(lines)
     graph = [[float("inf")] * q_nodes for i in range(q_nodes)]
     graph[0][0] = 0
-    for i in range(1, q_edges + 1):
-        node1, node2, weight = [int(e) for e in lines[i].split()]
+    for i in range(q_edges):
+        node1, node2, weight = next(lines)
         graph[node1][node2] = weight
         graph[node2][node1] = weight
-    nodes = [int(lines[i]) for i in range(q_edges + 1, q_edges + q_nodes + 1)]
+    nodes = [next(lines)[0] for i in range(q_nodes)]
 
     weights = [0] + [float("inf") for i in range(q_nodes - 1)]
     queue = PriorityQueue()
