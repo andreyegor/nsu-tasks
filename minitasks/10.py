51e25a59951218a8b44fc255fbbe6d72028a0dab
Egor <doesnoexist@gmail.com>
2023-11-08 18:30:28 +0700

    mini 10 not int only

--- minitasks/10.py
+++ minitasks/10.py
@@ -2,22 +2,31 @@ from collections import OrderedDict
 
 
 class LRUCache:
-    def __init__(self, capacity: int):
-        self.cache = OrderedDict()
-        self.capacity = capacity
+    def __init__(self, capacity=16):
+        self._cache = OrderedDict()
+        self._capacity = capacity
 
-    def get(self, key: int) -> int:
+    def get(self, key):
         try:
-            self.cache.move_to_end(key)
-            return self.cache[key]
+            self._cache.move_to_end(key)
+            return self._cache[key]
         except:
-            return -1
+            return None
 
-    def put(self, key: int, value: int) -> None:
+    def put(self, key, value):
         try:
-            self.cache.move_to_end(key)
+            self._cache.move_to_end(key)
         except:
             pass
-        self.cache[key] = value
-        if len(self.cache) > self.capacity:
-            self.cache.popitem(last=False)
+        self._cache[key] = value
+        if len(self._cache) > self._capacity:
+            self._cache.popitem(last=False)
+
+
+cache = LRUCache(3)
+cache.put("1", "a")
+cache.put("2", "b")
+cache.put("3", "c")
+print(cache.get("1"))
+cache.put("4", "d")
+print(cache.get("2"))
