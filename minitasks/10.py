17f02f28bb11da202b6a0d16f8d18f6f291ece53
Egor Andreev <70887089+andreyegor@users.noreply.github.com>
2023-11-08 15:12:40 +0700

    mini 10 solution

---
+++ minitasks/10.py
@@ -0,0 +1,21 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        try:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        except: 
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        try:
+            self.cache.move_to_end(key)
+        except: pass
+        self.cache[key] = value
+        if len(self.cache)>self.capacity:
+            self.cache.popitem(last=False)
+        
