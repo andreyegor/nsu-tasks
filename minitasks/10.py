c12c1833a95d3161eed2833177fadde6f3bf4010
Egor <doesnoexist@gmail.com>
2023-11-08 18:36:27 +0700

    mini 10 faster

--- minitasks/10.py
+++ minitasks/10.py
@@ -3,24 +3,21 @@ from collections import OrderedDict
 
 class LRUCache:
     def __init__(self, capacity=16):
-        self._cache = OrderedDict()
-        self._capacity = capacity
+        self.cache = OrderedDict()
+        self.capacity = capacity
 
     def get(self, key):
         try:
-            self._cache.move_to_end(key)
-            return self._cache[key]
+            self.cache.move_to_end(key)
+            return self.cache[key]
         except:
             return None
 
     def put(self, key, value):
-        try:
-            self._cache.move_to_end(key)
-        except:
-            pass
-        self._cache[key] = value
-        if len(self._cache) > self._capacity:
-            self._cache.popitem(last=False)
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache)>self.capacity:
+            self.cache.popitem(last=False)
 
 
 cache = LRUCache(3)
