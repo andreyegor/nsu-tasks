5576504112afa4e6a7f3144d530944ad58538e95
Egor <doesnoexist@gmail.com>
2023-11-13 16:59:22 +0700

    decorator

--- minitasks/11.py
+++ minitasks/11.py
@@ -1,21 +1,49 @@
 class Counter:
-    def __init__(self, initial_count=0,step=1):
+    def __init__(self, initial_count=0, step=1):
         self.count = initial_count
         self.step = step
+
     def increment(self):
         self.count += self.step
 
 
-class Singleton():
+class Singleton:
     __created__ = None
+
     def __new__(cls, *args, **kwargs):
         if cls.__created__ == None:
             cls.__created__ = super().__new__(cls, *args, **kwargs)
         return cls.__created__
-    
+
+
 class GlobalCounter(Singleton, Counter):
     pass
 
+
 gc1 = GlobalCounter()
 gc2 = GlobalCounter()
-assert id(gc1) == id(gc2) # True
+assert id(gc1) == id(gc2)  # True
+
+
+def singleton(cls):
+    cls.__created__ = None
+    cls.__default_new__ = cls.__new__
+
+    def new(cls, *args, **kwargs):
+        if cls.__created__ == None:
+            cls.__created__ = cls.__default_new__(cls, *args, **kwargs)
+        return cls.__created__
+
+    cls.__new__ = new
+
+    return cls
+
+
+@singleton
+class OtherGlobalCounter(Counter):
+    pass
+
+
+gc1 = OtherGlobalCounter()
+gc2 = OtherGlobalCounter()
+assert id(gc1) == id(gc2)  # True
