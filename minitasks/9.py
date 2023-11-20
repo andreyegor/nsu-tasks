0687a1aeb24da8c6f183e9cb336320e1ed51eb55
Egor <doesnoexist@gmail.com>
2023-11-20 16:21:13 +0700

    mini 9 fix

--- minitasks/9.py
+++ minitasks/9.py
@@ -6,7 +6,7 @@ def format_table(benchmarks, algos, results):
     heading = ["Benchmark", *algos]
     lines = [[benchmarks[i], *results[i]] for i in range(len(benchmarks))]
     columns_width = [
-        len(max(heading[i], *[line[i] for line in lines])) for i in range(len(heading))
+        max(len(str(heading[i])), *[len(str(line[i])) for line in lines]) for i in range(len(heading))
     ]
 
     normalized_heading = [
@@ -19,13 +19,13 @@ def format_table(benchmarks, algos, results):
         print("|" + "|".join(line) + "|")
 
 
+# format_table(
+#     ["best case", "worst case"],
+#     ["quick sort", "merge sort", "bubble sort"],
+#     [[1.23, 1.56, 2.0], [3.3, 2.9, 3.9]],
+# )
 format_table(
-    ["best case", "worst case"],
+    ["b", "t"],
     ["quick sort", "merge sort", "bubble sort"],
-    [[1.23, 1.56, 2.0], [3.3, 2.9, 3.9]],
-)
-format_table(
-    ["best case", "the worst case"],
-    ["quick sort", "merge sort", "bubble sort"],
-    [[1.23, 1.56, 2.0], [3.3, 2.9, 3.9]],
+    [[1.23, 1.56, 2.0], [3.3, 2.9, 3000000000000000000]],
 )
