ba2fff5da26fd691ccb9d6095acdff219e8e03d1
Egor <doesnoexist@gmail.com>
2024-02-09 19:34:43 +0700

    algo mini 1 expl

--- Algo/1.py
+++ Algo/1.py
@@ -1,20 +1,20 @@
-def divide(a, b):
+def divide(a, b): # a - n разрядное b - m разрядное, a - делимое b - делитель
     ln = 0
-    while a >= b:
+    while a >= b: # не больше n раз
         b <<= 1
         ln += 1
     b >>= 1
     out = 0
-    for i in range(ln):
+    for i in range(ln): #ln не превышает n
         t = 0
         cnt = 0
-        while t <= a:
+        while t <= a: # не больше m раз
             t += b
             cnt += 1
         cnt -= 1 if cnt != 0 and a != t else 0
 
         k = cnt
-        while True:
+        while True: # также не больше m раз
             out <<= 1
             k >>= 1
             if not k:
@@ -23,12 +23,13 @@ def divide(a, b):
         out += cnt
         a -= t - b
         b >>= 1
-    return (out, a)
+    return (out, a) # значение и остаток от деления
 
+#2n+2m = O(n+m)
 
 if __name__ == "__main__":
     divide(2, 1)
-    for a in range(0, 10):
-        for b in range(1, 10):
+    for a in range(0, 100):
+        for b in range(1, 100):
             if divide(a, b) != (a // b, a % b):
                 print(f"{a}/{b}!={divide(a,b)}")
