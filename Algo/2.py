ba24c46c23b1451b9ce30a0d22b02442ea478907
Egor <doesnoexist@gmail.com>
2024-02-10 18:38:59 +0700

    algo mini 2 new karatsuba

--- Algo/2.py
+++ Algo/2.py
@@ -1,49 +1,34 @@
-def plus(n1, n2):
-    return str(int(n1) + int(n2))
+from math import ceil
 
 
-def minus(n1, n2):
-    return str(int(n1) - int(n2))
+def bit_len(n: int) -> int:
+    out = n != 0
+    while n := n >> 1:
+        out += 1
+    return out
 
 
-def multiply(n1, n2):
-    out = 0
-    n2 = int(n2)
-    for i in range(int(n1)):
-        out += n2
-    return str(out)
+def karatsuba(n1, n2):
+    n1, n2 = sorted((n1, n2), key=bit_len, reverse=True)
+    if n1 >> 1 == 0:
+        return n1 and n2
 
-
-def karatsuba(n1: str, n2: str) -> str:
-    n1, n2 = sorted([n1, n2], key=len)
-    if len(n2) == 1:
-        return multiply(n1, n2)
-    if len(n2) % 2 != 0:
-        n2 = "0" + n2
-    n1 = n1.rjust(len(n2), "0")
-
-    a, b = n1[: len(n1) // 2], n1[len(n1) // 2 :]
-    c, d = n2[: len(n2) // 2], n2[len(n2) // 2 :]
+    half_ln = ceil(bit_len(n1) / 2)
+    a = n1 >> half_ln
+    b = n1 ^ (a << half_ln)
+    c = n2 >> half_ln
+    d = n2 ^ (c << half_ln)
 
     k1 = karatsuba(a, c)
     k2 = karatsuba(b, d)
-    k3 = karatsuba(plus(a, b), plus(c, d))
-    k4 = minus(minus(k3, k2), k1)
-    out = plus(k1 + "0" * len(n1), plus(k4 + "0" * (len(n1) // 2), k2))
-
-    assert out == multiply(n1, n2)
+    k3 = karatsuba(a + b, c + d)
+    k4 = k3 - k2 - k1
+    out = (k1 << half_ln * 2) + (k4 << half_ln) + k2
 
     return out
 
 
 if __name__ == "__main__":
-    tests = [
-        [["1234", "5678"], "7006652"],
-        [["1234", "0"], "0"],
-        [["1234", "1"], "1234"],
-        [["1234", "2"], "2468"],
-    ]
-    for inp, outp in tests:
-        if (res := karatsuba(*inp)) != outp:
-            print(f"{inp[0]} * {inp[1]} = {res}, but {outp} expected")
-    # assert all(multiply(*a) == b for a, b in tests)
+    for i in range(100):
+        for j in range(100):
+            assert i * j == karatsuba(i, j)
