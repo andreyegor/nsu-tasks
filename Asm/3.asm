feeb4b46af99254dc08a20da104c66382edd7067
Egor <doesnoexist@gmail.com>
2024-03-19 00:58:37 +0700

    asm 3 other fix

--- Asm/3.asm
+++ Asm/3.asm
@@ -13,7 +13,8 @@ main:
 	mv s2 a0
 	read
 	mv s3 a0
-	
+	writi '\n'
+
 	beq s3 s8 add
 	beq s3 s9 sub
 	beq s3 s10 and
@@ -21,42 +22,58 @@ main:
 	exit 1
 add:
 	add s4 s1 s2
-	j write
+	j continue
 sub:
 	sub s4 s1 s2
-	j write
+	j continue
 and:
 	and s4 s1 s2
-	j write
+	j continue
 or: 
 	or s4 s1 s2
-	j write
-write:
+	j continue
+continue:
 	writi '\n'
 	mv a0 s4
-	call count_bytes_bits
-	mv s5 a0
-write_loop:
-	srl a0 s4 s5
-	call num_to_ascii
-	write
-	sll a0 a0 s5
-	sub s4 s4 a0
-	beq zero s5 end
-	addi s5 s5 -4
-	j write_loop
-end:
+	call write_num
 	exit 0
 
 #functions
-ascii_to_num: #a0-->a0 also use: t5
-	li t5 16
+write_num: # a0-> also use t0 t1
+	mv t0 a0
+
+	push ra
+	push t0
+	call count_bytes_bits
+	pop t0
+	pop ra
+
+	mv t1 a0
+_write_num_loop:
+	srl a0 t0 t1
+	
+	push_3 ra t0 t1
+	call num_to_ascii
+	pop_3 ra t0 t1
+
+	write
+	sll a0 a0 t1
+	sub t0 t0 a0
+	beq zero t1 _write_num_end
+	addi t1 t1 -4
+	j _write_num_loop
+_write_num_end:
+	ret
+
+
+ascii_to_num: #a0-->a0 also use: t0
+	li t0 16
 	addi a0 a0 -48
 	blt a0 zero _ascii_to_num_invalid_exit
-	blt a0 t5 _ascii_to_num_quit
+	blt a0 t0 _ascii_to_num_quit
 	
 	addi a0 a0 -7
-	blt a0 t5 _ascii_to_num_quit
+	blt a0 t0 _ascii_to_num_quit
 	
 	addi a0 a0 -32
 	blt a0 zero _ascii_to_num_invalid_exit
@@ -67,23 +84,27 @@ _ascii_to_num_quit:
 	ret 
 	
 
-read_num: #-->a0 also use: t2 t3 t4
-	li t2 0
-	li t3 '\n'
+read_num: #-->a0 also use: t0 t1 t2
+	li t0 0
+	li t1 '\n'
+	li t2 0x10000000
 _read_num_loop:
 	read
-	beq a0 t3 _read_num_quit
+	beq a0 t1 _read_num_quit
+	bge t0 t2 _read_num_error
 	
-	mv t4 ra
+	push_4 ra t0 t1 t2
 	call ascii_to_num
-	mv ra t4
-	
-	slli t2 t2 4
-	add t2 t2 a0
+	pop_4 ra t0 t1 t2
+
+	slli t0 t0 4
+	add t0 t0 a0
 	j _read_num_loop
 _read_num_quit:
-	mv a0 t2
+	mv a0 t0
 	ret
+_read_num_error:
+	exit 1
 	
 count_bytes_bits: #a0-->a0, also use t0
 	mv t0 a0
--- Asm/things.asm
+++ Asm/things.asm
@@ -20,3 +20,55 @@
 	li a0 %code
 	syscall 93
 .end_macro
+
+.macro push %r1
+	addi sp, sp, -4
+	sw %r1, 0(sp)
+.end_macro
+
+.macro pop %r1
+	lw %r1, 0(sp)
+	addi sp, sp, 4
+.end_macro
+
+.macro push_2 %r1 %r2
+	addi sp, sp, -8
+	sw %r1, 0(sp)
+	sw %r2, 4(sp)
+.end_macro
+
+.macro pop_2 %r1 %r2
+	lw %r1, 0(sp)
+	lw %r2, 4(sp)
+	addi sp, sp, 8
+.end_macro
+
+.macro push_3 %r1 %r2 %r3
+	addi sp, sp, -12
+	sw %r1, 0(sp)
+	sw %r2, 4(sp)
+	sw %r3, 8(sp)
+.end_macro
+
+.macro pop_3 %r1 %r2 %r3
+	lw %r1, 0(sp)
+	lw %r2, 4(sp)
+	lw %r3, 8(sp)
+	addi sp, sp, 12
+.end_macro
+
+.macro push_4 %r1 %r2 %r3 %r4
+	addi sp, sp, -16
+	sw %r1, 0(sp)
+	sw %r2, 4(sp)
+	sw %r3, 8(sp)
+	sw %r4, 12(sp)
+.end_macro
+
+.macro pop_4 %r1 %r2 %r3 %r4
+	lw %r1, 0(sp)
+	lw %r2, 4(sp)
+	lw %r3, 8(sp)
+	lw %r4, 12(sp)
+	addi sp, sp, 16
+.end_macro
